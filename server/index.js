46bc4993e37f172c40c4956be209b3150717609f
dawum nam <dawumnam@gmail.com>
2020-08-15 22:10:15 -0800

    asd

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+io.on('connection', (socket) => {
+    socket.on('join',({name, room},callback) => {
+      const { error, user } = addUser({id: socket.id, name, room});
+      if (error) callback(error);
+
+      socket.join(user.room);
+
+      socket.emit('message',{ user: 'admin', text:`${user.name}, welcome to the room ${user.room}`});
+      socket.broadcast.to(user.room).emit('message', { user: 'admin', text:`${user.name} has joined!`});
+
+      callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+      const user = getUser(socket.id);
+      io.to(user.room).emit('message', {user: user.name, text: message});
+      callback();
+    })
+    socket.on('disconnect',() => {
+       console.log(`User had left`) ;
+    });
+});
+
+const router = require('./router');
+
+app.use(router);
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`)  
+});
+
+
